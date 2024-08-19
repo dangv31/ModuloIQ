5af20289b9df84db137f6f599c49dbf58d461207
tvargasl <126362144+tvargasl@users.noreply.github.com>
2024-08-19 13:29:11 -0500

    Update Ver_Sede.py
    Actualizar Ver_Sede.py a interfaz

--- src/ui_main/gestion_sedes/Ver_Sede.py
+++ src/ui_main/gestion_sedes/Ver_Sede.py
@@ -1,28 +1,51 @@
+import tkinter as tk
+
 from src.base_datos.Gestor_Base import Gestor_Base
 
 
 class Ver_Sede:
     @classmethod
-    def ver_sede(cls, cuenta):
-        nombre_sede = input("Ingrese el nombre de la sede: ")
-        sede_l = Gestor_Base.buscar_objeto(nombre_sede, "Sede")
-        while True:
-            if sede_l == None:
-                print("Esa sede no existe")
-                from src.ui_main.Menu_inicial import Menu_inicial
-                return Menu_inicial.menu_inicial_Administrativo(cuenta)
+    def ver_sede(cls, cuenta, ventana):
+        def provisional():
+            volver_menu(cuenta,ventana)
+
+        def buscar_sede():
+            nombre_sede = entradaNombreSede.get()
+            sede_1 = Gestor_Base.buscar_objeto(nombre_sede, "Sede")
+
+            if sede_1 == None:
+                tk.messagebox.showerror("Error", "Esta sede no existe.")
             else:
-                break
-        id = sede_l[0]
-        sede = sede_l[1]
-        sede.imprimir_info()
-        print("Observaciones: ")
-        for observacion in sede.observaciones:
-            print(observacion)
-        entrada = input("Ingrese cualquier caracter para volver al menu incial: ")
-        if entrada:
-            from src.ui_main.Menu_inicial import Menu_inicial
-            if "Administrativo" in cuenta.rol:
-                Menu_inicial.menu_inicial_Administrativo(cuenta)
-            else:
-                Menu_inicial.menu_inicial_Clinico(cuenta)
+                for item in frame_info.winfo_children():
+                    item.destroy()
+                id = sede_1[0]
+                sede = sede_1[1]
+                sede.imprimir_info(frame_info)
+
+                tk.Label(frame_info, text="Observaciones: ").pack()
+
+                for observacion in sede.observaciones:
+                    tk.Label(frame_info, text=observacion).pack()
+
+        
+        from src.ui_main.herramientas.volver_menu import volver_menu
+        from src.ui_main.herramientas.imprimir_titulo import imprimir_titulo
+
+        imprimir_titulo(ventana, "Ver Sede:")
+
+        frame_buscar = tk.Frame(ventana)
+        frame_buscar.pack(pady=20)
+
+        frame_info = tk.Frame(ventana)
+        frame_info.pack(pady=20)
+
+        textNombreSede = tk.Label(frame_buscar, text="Nombre De Sede:")
+        textNombreSede.pack(pady=5)
+        entradaNombreSede = tk.Entry(frame_buscar)
+        entradaNombreSede.pack(pady=5)
+
+        buscar = tk.Button(frame_buscar, text="Buscar", command= buscar_sede)
+        buscar.pack(side=tk.TOP, padx=0)
+
+        salir = tk.Button(ventana, text="Volver al menu", command= provisional)
+        salir.pack(side=tk.LEFT, padx=600)
